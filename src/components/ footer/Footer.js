04c2dbefc34934df1cd0f2e429baf26347350ed4
Ruslan Hrybchuk <ruslan.hrybchuk@gmail.com>
2020-11-14 23:20:17 +0200

    minor ui tweaks

--- src/components/ footer/Footer.js
+++ src/components/ footer/Footer.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Footer.css';
-import { FaPlay, FaAngleLeft, FaAngleRight} from 'react-icons/fa';
+import { AiFillBackward, AiFillForward, AiOutlinePause} from 'react-icons/ai';
+import { ImPlay3 } from "react-icons/im";
 
 function Footer(props) {
     return (
         <div className='footer-wrapper'>
             <div className='play-buttons'>
-                <div className='previousTrack button'><FaAngleLeft color='#CACACA' size='24px'/></div>
-                <div className='play button'><FaPlay color='#D3D3D3' size='18px' /></div>
-                <div className='nextTrack button'><FaAngleRight color='#CACACA' size='24px'/></div>
+                <div className='previousTrack button'><AiFillBackward color='#CACACA' size='24px'/></div>
+                <div className='play button'><ImPlay3 color='#D3D3D3' size='28px' /></div>
+                <div className='nextTrack button'><AiFillForward color='#CACACA' size='24px'/></div>
             </div>
             <div className='timeline'>
                 <div className='playhead'></div>
